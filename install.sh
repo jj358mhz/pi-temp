73f46a3cc1ae46a1ad565ccf77e3eb9e7c286102
Jeff Johnston <jeff.johnston@edgecast.com>
2023-03-12 12:08:35 -0700

    ChatGPT Updates

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -x
+
+# VERSION = [1.0.0] - 2023-03-11
+
+# This script installs the tempcheck.sh script, config file, and necessary dependencies.
+
+# File Locations for Raspberry Pi (Debian based)
+# /usr/local/bin/tempcheck.sh   ( script file )
+# /etc/tempcheck/tempcheck.conf ( config file )
+
+# Dependencies: curl
+DEPS="curl"
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y $DEPS
+
+# Create directories for config file
+sudo mkdir -p /etc/tempcheck
+
+# Copy the tempcheck.sh script to /usr/local/bin/
+sudo cp tempcheck.sh /usr/local/bin/tempcheck.sh
+sudo chmod +x /usr/local/bin/tempcheck.sh
+
+# Prompt user for Slack webhook
+read -p "Enter your Slack webhook: " SLACK_WEBHOOK
+
+# Prompt user for Slack channel name
+read -p "Enter your Slack channel name: " SLACK_CHANNEL
+
+# Prompt user for device title
+read -p "Enter the title of your device: " TITLE
+
+# Create config file
+echo "#  This script config file configures the tempcheck script that reads the Broadcom SoC temperature value and" | sudo tee /etc/tempcheck/tempcheck.conf
+echo "#  shuts down if it exceeds a particular value." | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "#  80ºC is the maximum allowed for a Raspberry Pi." | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "" | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "# Enter your Slack webhook" | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "SLACK_WEBHOOK=\"$SLACK_WEBHOOK\"" | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "" | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "#Enter your Slack channel name" | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "SLACK_CHANNEL=\"$SLACK_CHANNEL\"" | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "" | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "# Enter the title of your device" | sudo tee -a /etc/tempcheck/tempcheck.conf
+echo "TITLE=\"$TITLE\"" | sudo tee -a /etc/tempcheck/tempcheck.conf
+
+# Add cronjob to run the script every 5 minutes
+(crontab -l 2>/dev/null; echo "*/5 * * * * root /usr/local/bin/tempcheck.sh >/dev/null 2>&1") | sudo crontab -
+
+echo "Installation complete!"
--- tempcheck.sh
+++ tempcheck.sh
@@ -1,62 +1,37 @@
 #!/usr/bin/env bash
 
-set -x
+# tempcheck.sh - A script that checks the temperature of a Raspberry Pi and shuts it down if it exceeds a particular value.
+# Author: Jeff Johnston <jj358mhz@gmail.com>
 
-# VERSION = [2.0.0] - 2023-03-11
+readonly VERSION="2.1.0"
+readonly CONFIGFILE="/etc/tempcheck/tempcheck.conf"
+readonly EMOJI=":fire:"
 
-#  This script reads the Broadcom SoC temperature value and shuts down if it
-#  exceeds a particular value.
-#  80ºC is the maximum allowed for a Raspberry Pi.
-
-# Copyright (C) 2014-2023 Jeff Johnston <jj358mhz@gmail.com>
-
-# This script work with a secondary script called ( post_to_slack )
-
-# File Locations for Raspberry Pi (Debian based)
-### /usr/local/bin/tempcheck.sh   ( this file )
-### /etc/tempcheck/tempcheck.conf ( config file )
-
-# Schedule a cronjob to run every 5 minutes
-# sudo crontab -e and paste the entry below
-# */5 * * * * root /usr/local/bin/tempcheck.sh >/dev/null 2>&1
-
-# Example of job definition:
-# .---------------- minute (0 - 59)
-# |  .------------- hour (0 - 23)
-# |  |  .---------- day of month (1 - 31)
-# |  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ...
-# |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
-# |  |  |  |  |
-# *  *  *  *  * user-name  command to be executed
-
-# PATH added in case it is not set when cron runs @reboot
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-# Make config changes to the following file...
-CONFIGFILE=/etc/tempcheck/tempcheck.conf
-
-# Check and load the conf file
-[ -f "$CONFIGFILE" ] && . $CONFIGFILE
-
-##################################################################
-
-# Slack emoji (you can change to suit your needs)
-EMOJI=":fire:"
+# Load config file
+if [ -f "$CONFIGFILE" ]; then
+  . "$CONFIGFILE"
+else
+  echo "Config file not found: $CONFIGFILE"
+  exit 1
+fi
 
 # Get the reading from the sensor and strip the non-number parts
-SENSOR="$(/usr/bin/vcgencmd measure_temp | cut -d "=" -f2 | cut -d "'" -f1)"
+readonly SENSOR="$(/usr/bin/vcgencmd measure_temp | cut -d "=" -f2 | cut -d "'" -f1)"
 
-# 80ºC is the maximum allowed for a Raspberry Pi.
-# How hot will we allow the SoC to get in Celsius?
-MAX="78"
+# Set the maximum allowed temperature for a Raspberry Pi
+readonly MAX="78"
 
 # -gt only deals with whole numbers, so round it.
-TEMP="$( (/usr/bin/printf "%.0f\n" "${SENSOR}") )"
+readonly TEMP="$( (/usr/bin/printf "%.0f\n" "${SENSOR}") )"
 
 if [ "${TEMP}" -gt "${MAX}" ] ; then
   echo "${TEMP}ºC is too hot!" # This will be mailed to root if called from cron
-  curl -X POST -H 'Content-type: application/json' --data "{\"attachments\": [{\"emoji\": \"${EMOJI}\", \"fallback\": \"${TEMP}ºC is too hot!\", \"color\": \"#FF0000\", \"pretext\": \"${TITLE}\", \"author_name\": \"${TEMP}ºC is too hot!\", \"footer\": \"Slack API\", \"ts\": \"$(date +%s)\"}]}" "${SLACK_WEBHOOK}" # Send a message to Slack
-  #sudo /sbin/shutdown -h now # Uncomment the line below to shut down the Pi
+  # Send a message to Slack
+  if ! curl -sSf -X POST -H 'Content-type: application/json' --data "{\"attachments\": [{\"emoji\": \"${EMOJI}\", \"fallback\": \"${TEMP}ºC is too hot!\", \"color\": \"#FF0000\", \"pretext\": \"${TITLE}\", \"author_name\": \"${TEMP}ºC is too hot!\", \"footer\": \"Slack API\", \"ts\": \"$(date +%s)\"}]}" "${SLACK_WEBHOOK}"; then
+    echo "Failed to send Slack message"
+    exit 1
+  fi
+  sudo /sbin/shutdown -h now # Comment out the line for testing
 else
   exit 0
 fi
